626e35e2477e3b95bbc4033f2efea9f7179523a2
naveenkarthika <yuvannaveen65@gmail.com>
2022-02-19 18:41:05 +0530

    State component changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import Practices from 'components/Practices';
 import ContextApp from 'components/useContext';
+import State from 'StateManagement';
 
-import { useReducer } from 'react';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'plus':
-      return state + 1;
-    case 'minus':
-      return state - 1;
-    default:
-      return state;
-  }
-}
 
 function App() {
-
-  const [state, dispatch] = useReducer(reducer, 0);
-
   return (
     <div>
-      <div>
-        <h2>State: {state} </h2>
-      </div><br />
-      <button onClick={() => dispatch({ type: 'plus'})}>Plus</button>
-      <button onClick={() => dispatch({ type: 'minus'})}>Minus</button>
-      <br />
       <Practices />
+      <State />
     </div>
   );
 }
