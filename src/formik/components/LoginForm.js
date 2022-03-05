8175964a8d784a0e08ea0ee596eb98967031bfe1
naveenkarthika <yuvannaveen65@gmail.com>
2022-03-05 19:42:31 +0530

    Error msg changes

--- src/formik/components/LoginForm.js
+++ src/formik/components/LoginForm.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import TextError from './TextError'
 
 function LoginForm() {
     const initialValues = {
         name: '',
         email: '',
         password: '',
+        comment: '',
+        address: ''
       }
     
     const onSubmit = (values) => {
@@ -16,7 +19,8 @@ function LoginForm() {
     const validationSchema = Yup.object({
         name: Yup.string().required('Required!'),
         email: Yup.string().email('Invalid Email').required('Required!'),
-        password: Yup.string().required('Required!')
+        password: Yup.string().required('Required!'),
+        address: Yup.string().required('Required!')
     })
 
   return (
@@ -28,7 +32,7 @@ function LoginForm() {
                     type="text" 
                     name="name" 
                     id="name" />
-                <ErrorMessage name="name"/>
+                <ErrorMessage name="name" component={TextError}/>
             </div>
             <div className="formControl">
                 <label htmlFor="email">Email</label>
@@ -36,7 +40,9 @@ function LoginForm() {
                     type="email" 
                     name="email" 
                     id="email" />
-                <ErrorMessage name="email"/>
+                <ErrorMessage name="email">
+                    {(errorMsg) => <div className="errors">{errorMsg}</div>}
+                </ErrorMessage>
             </div>
             <div className="formControl">
                 <label htmlFor="password">Password</label>
@@ -44,7 +50,30 @@ function LoginForm() {
                     type="password" 
                     name="password" 
                     id="password" />
-                <ErrorMessage name="password"/>
+                <ErrorMessage name="password" component={TextError} />
+            </div>
+            <div className="formControl">
+                <label htmlFor="comment">Comment</label>
+                <Field 
+                    as="textarea" 
+                    name="comment"
+                    placeholder="Enter the user comment" 
+                    rows="5"
+                    id="comment" />
+            </div>
+            <div className="formControl">
+                <label htmlFor="address">Address</label>
+                <Field name="address" id="address" >
+                    {(props) => {
+                        const { field, form, meta } = props;
+                        return (
+                            <div>
+                                <input type="text" id="address" {...field} />
+                                {meta.touched && meta.error ? <div className="errors">{meta.error}</div> : null}
+                            </div>
+                        )
+                    }}
+                </Field>
             </div>
             <button type="submit">Submit</button>
         </Form>
